7acc845c068aa70743be2109c8599cd24e83a8e2
abhishek747484 <poudelabhishek73@gmail.com>
2026-06-08 13:43:37 +0545

    fix backend submodule

---
+++ backend/src/models/usermodels.js
@@ -0,0 +1,32 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true
+    },
+    password:{
+        type: String,
+        required: true,
+        unique: false,
+        lowercase:true,
+        minlength: 8,
+
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true
+    }
+},
+{
+    timestamps: true
+}
+);
+
+export const User = mongoose.model("User", userSchema);
