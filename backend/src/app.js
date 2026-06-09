9f5c41ef2d0e7cb55933e021c7bc002cfa8ca901
Hem Singh <hem98265@gmail.com>
2026-06-09 14:17:07 +0545

    route testing

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,7 @@
 import express from "express";
 const app = express();
 
+import userRoute from './routes/userroute.js';
+app.use('/api/users', userRoute);
+
 export default app;
--- backend/src/routes/userroute.js
+++ backend/src/routes/userroute.js
@@ -2,4 +2,22 @@ import {Router} from 'express';
 
 const router = Router();
 
+router.get('/', (req, res) => {
+    res.send('Get all users');
+}
+);
+
+router.post('/create', (req, res) => {
+    res.send('Create a new user');
+});
+
+router.get('/receive/:id', (req, res) => {
+    res.send(`Get user with id ${req.params.id}`);
+}); 
+
+router.delete('/delete/:id', (req, res) => {
+    res.send(`Delete user with id ${req.params.id}`);
+});     
+
+
 export default router;
